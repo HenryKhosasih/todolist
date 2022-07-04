d26df404db8f22e28e3f048439d41850ffa6f028
Henry Khosasih <henrylaurentkhosasih@gmail.com>
2022-07-05 00:24:51 +1000

    Add procfile, update gitignore, port and package

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const app = express();
-const localPort = 3000;
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+	port = 3000;
+}
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,15 +26,15 @@ const itemsSchema = {
 const Item = mongoose.model("item", itemsSchema);
 
 const item1 = new Item({
-	name: "Coding",
+	name: "Welcome to your todolist!",
 });
 
 const item2 = new Item({
-	name: "Cooking",
+	name: "Hit the + button to add a new item.",
 });
 
 const item3 = new Item({
-	name: "Working",
+	name: "<-- Hit this to delete an item.>",
 });
 
 const defaultItems = [item1, item2, item3];
@@ -133,6 +137,6 @@ app.get("/:customListName", function (req, res) {
 	});
 });
 
-app.listen(localPort, function () {
-	console.log(`ToDoList server is running at port ${localPort}`);
+app.listen(port, function () {
+	console.log(`ToDoList server is running at port ${port}`);
 });
